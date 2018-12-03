f4a2f7f4b98b99fe74b9afe7a4f295705834e34b
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2018-12-03 22:53:03 +0000

    Test for day 3

--- run_tests.sh
+++ run_tests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 ./01_frequency_changes/clever.py | diff 01_frequency_changes/output.txt -
 ./02_inventory/checksum.py | diff 02_inventory/output.txt -
+./03_fabric_cutting/overlapping.py | diff 03_fabric_cutting/output.txt -
