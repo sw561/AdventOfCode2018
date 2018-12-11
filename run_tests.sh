5ba1d55e87f11f361527e032a536163928038313
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2018-12-11 15:45:55 +0000

    Test for day 11

--- run_tests.sh
+++ run_tests.sh
@@ -4,3 +4,9 @@
 ./03_fabric_cutting/overlapping.py | diff 03_fabric_cutting/output.txt -
 ./04_guards/guard.py | diff 04_guards/output.txt -
 ./05_polymer/polymer.py | diff 05_polymer/output.txt -
+./06_voronoi/chronal.py | diff 06_voronoi/output.txt -
+./07_gantt/tasks.py | diff 07_gantt/output.txt -
+./08_metadata/metadata.py | diff 08_metadata/output.txt -
+./09_marble/marble.py | diff 09_marble/output.txt -
+./10_stars/stars_align.py | diff 10_stars/output.txt -
+./11_fuel_cell/power.py | diff 11_fuel_cell/output.txt -
