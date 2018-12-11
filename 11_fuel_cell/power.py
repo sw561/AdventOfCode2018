af400a81821fd3ebeab09a11fa4b89dfaa4b3736
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2018-12-11 15:24:34 +0000

    Day 11 part 2

--- 11_fuel_cell/power.py
+++ 11_fuel_cell/power.py
@@ -6,20 +6,46 @@ def power(data, x, y):
     p = (p % 1000) // 100 - 5
     return p
 
-def find(data):
-    # Find 3x3 square within 300x300 grid with largest total power
+def construct_grid(data):
+    # each entry in grid is sum of cells above and to the left
+    #
+    # Can thus find total power within a square using just four entries.
 
-    for i in range(297):
-        for j in range(297):
-            p = sum(power(data, i+ki, j+kj) for ki in range(3) for kj in range(3))
-            yield p, i, j
+    # Top row and left hand column are zero, for easy algorithm design
+    grid = [[0]*301 for _ in range(301)]
 
-def part1(data):
-    return max(find(data))
+    for j in range(1, 300):
+        for i in range(1, 300):
+            grid[j][i] = grid[j-1][i] + grid[j][i-1] - grid[j-1][i-1] + power(data, i, j)
+
+    return grid
+
+def power_square(grid, s, x, y):
+
+    # bottom right corner
+    xr, yr = x + s - 1, y + s - 1
+
+    return grid[yr][xr] - grid[yr][x-1] - grid[y-1][xr] + grid[y-1][x-1]
+
+def find(grid, s):
+    # Find square of side s with largest total power, return coords of top left corner
+
+    return max((power_square(grid, s, x, y), x, y) for x in range(300-s) for y in range(300-s))
+
+def part1(grid):
+    return find(grid, 3)
+
+def part2(grid):
+    return max((*find(grid, s), s) for s in range(300))
 
 if __name__=="__main__":
     with open("11_fuel_cell/input.txt", 'r') as f:
         data = int(f.read())
 
-    ret = part1(data)
+    grid = construct_grid(data)
+
+    ret = part1(grid)
+    print(",".join(map(str, ret[1:])))
+
+    ret = part2(grid)
     print(",".join(map(str, ret[1:])))
--- 11_fuel_cell/test.py
+++ 11_fuel_cell/test.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 
-from power import power, part1
+from power import power, construct_grid, power_square, find, part2
 
 def assertEqual(x, y):
     try:
@@ -11,18 +11,41 @@ def assertEqual(x, y):
 
 def test1():
     tests = [
-    (  3,  5,  8,  4),
-    (122, 79, 57, -5),
-    (217,196, 39,  0),
-    (101,153, 71,  4),
+    ( 8,   3,  5,  4),
+    (57, 122, 79, -5),
+    (39, 217,196,  0),
+    (71, 101,153,  4),
     ]
 
-    for x, y, data, o in tests:
-        assertEqual(power(data, x, y), o)
+    for data, x, y, p in tests:
+        assertEqual(power(data, x, y), p)
+
+def power_square_brute(data, s, x, y):
+    return sum(power(data, xi, yi) for xi in range(x, x+s) for yi in range(y, y+s))
 
 def test2():
-    assertEqual(part1(18), (29, 33, 45))
-    assertEqual(part1(42), (30, 21, 61))
+    s = 3
+    tests = [
+    (18, 33, 45, 29),
+    (42, 21, 61, 30),
+    ]
+
+    for data, x, y, p in tests:
+        grid = construct_grid(data)
+        assertEqual(power_square(grid, s, x, y), power_square_brute(data, s, x, y))
+        assertEqual(power_square(grid, s, x, y), p)
+        assertEqual(find(grid, s), (p, x, y))
+
+def test3():
+    tests = [
+    (18, 113,  90, 269, 16),
+    (42, 119, 232, 251, 12),
+    ]
+
+    for data, power, x, y, s in tests:
+        grid = construct_grid(data)
+        assertEqual(part2(grid), (power, x, y, s))
 
 test1()
 test2()
+test3()
