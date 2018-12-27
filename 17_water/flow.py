0111faeb9a6b1a316269f1462f89ef3a4cfce27f
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2018-12-27 19:04:17 +0100

    Solution for part 1 day 17

---
+++ 17_water/flow.py
@@ -0,0 +1,130 @@
+#!/usr/bin/env python3
+
+import re
+from collections import deque
+
+class Clay:
+    def __init__(self, a, b, c, vertical=True):
+        if vertical:
+            self.xmin = a
+            self.xmax = a
+            self.ymin = b
+            self.ymax = c
+
+        else:
+            self.ymin = a
+            self.ymax = a
+            self.xmin = b
+            self.xmax = c
+
+    def __iter__(self):
+        for x in range(self.xmin, self.xmax+1):
+            for y in range(self.ymin, self.ymax+1):
+                yield x, y
+
+class Grid:
+    def __init__(self, clays):
+        self.xmin = min(clay.xmin for clay in clays) - 1
+        self.xmax = max(clay.xmin for clay in clays) + 1
+        self.ymax = max(clay.ymax for clay in clays)
+        self.ymin = min(clay.ymin for clay in clays)
+
+        self.grid = [['.'] * (self.xmax - self.xmin + 1) for _ in range(self.ymax + 1)]
+
+        for clay in clays:
+            for x, y in clay:
+                self.set(x, y, '#')
+
+    def get(self, x, y):
+        return self.grid[y][x - self.xmin]
+
+    def set(self, x, y, val):
+        self.grid[y][x - self.xmin] = val
+
+    def count_water(self):
+        return sum(char in ['|', '~'] for row in self.grid[self.ymin:] for char in row)
+
+    def __str__(self):
+        return "\n".join("".join(row) for row in self.grid) + "\n--------------------------"
+
+def flood_fill(grid, x, y):
+
+    # print("Calling flood_fill with x, y = {}, {}".format(x, y))
+
+    # Go down as far as possible
+    while grid.get(x, y+1) != '#':
+        grid.set(x, y, '|')
+        y += 1
+        if y == grid.ymax:
+            grid.set(x, y, '|')
+            return []
+
+    overflow = []
+    done = False
+
+    while True:
+
+        # Expand horizontally from x, y
+        # print("Horizontal expansion from x, y = {}, {}".format(x, y))
+        grid.set(x, y, '~')
+
+        for inc_f in [lambda x: x-1, lambda x: x+1]:
+            xi = inc_f(x)
+            while grid.get(xi, y) != '#' and grid.get(xi, y+1) in ['#', '~']:
+                grid.set(xi, y, '~')
+                xi = inc_f(xi)
+
+            if grid.get(xi, y) == '.' and grid.get(xi, y+1) == '.':
+                grid.set(xi, y, '|')
+                overflow.append((xi, y))
+
+            elif grid.get(xi, y) == '|':
+                done = True
+
+        if overflow:
+            return overflow
+
+        if done:
+            return []
+
+        # No outflow, go up one square
+        y -= 1
+
+    return overflow
+
+def fill_water(grid):
+    grid.set(500, 0, '+')
+    # In heap store tuples (-y, x, y) since we want to largest y
+    d = deque([(500, 1)])
+
+    while d:
+        x, y = d.popleft()
+
+        overflow = flood_fill(grid, x, y)
+
+        for c in overflow:
+            d.append(c)
+
+        # with open("o", 'w') as g:
+        #     g.write(str(grid))
+
+        # print(d)
+        # input()
+
+if __name__=="__main__":
+    clays = []
+    pattern = re.compile("\\b\\d+\\b")
+    # with open("17_water/test_input.txt", 'r') as f:
+    with open("17_water/input.txt", 'r') as f:
+        for line in f:
+            x = re.findall(pattern, line)
+            clays.append(Clay(*map(int, x), vertical=line.startswith('x')))
+
+    grid = Grid(clays)
+    # print(grid)
+
+    fill_water(grid)
+    with open("o", 'w') as g:
+        g.write(str(grid))
+
+    print(grid.count_water())
