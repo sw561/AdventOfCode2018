49e3a602ae9cdac3e7be4e0b5ffc26e1a3eae2bd
Simon Wilkinson <simon_wilkinson92@hotmail.com>
2019-01-11 01:31:46 +0000

    Use A* algorithm, it is much faster and quite simple

--- 22_cave/cave.py
+++ 22_cave/cave.py
@@ -3,29 +3,23 @@
 from heapq import heappush, heappop
 
 def geological_index(x, y, targetx, targety, depth):
-    if x == 0 and y == 0:
-        return 0
-    if x == targetx and y == targety:
-        return 0
     if y == 0:
         return x * 16807
     if x == 0:
         return y * 48271
+    if x == targetx and y == targety:
+        return 0
 
     return erosion(x-1, y, targetx, targety, depth) *\
         erosion(x, y-1, targetx, targety, depth)
 
-def cache(f):
-    c = {}
-    def cached(*args):
-        if args not in c:
-            c[args] = f(*args)
-        return c[args]
-    return cached
+def erosion(x, y, targetx, targety, depth, cache={}):
+    if (x, y) in cache:
+        return cache[(x, y)]
 
-@cache
-def erosion(x, y, targetx, targety, depth):
-    return (geological_index(x, y, targetx, targety, depth) + depth) % 20183
+    ret = (geological_index(x, y, targetx, targety, depth) + depth) % 20183
+    cache[(x, y)] = ret
+    return ret
 
 def terrain(*args):
     return erosion(*args) % 3
@@ -58,17 +52,20 @@ def adjacent(time, x, y, tool):
         if t != tool:
             yield time+7, x, y, t
 
+def heuristic(time, x, y, targetx, targety):
+    return time + abs(targetx - x) + abs(targety - y)
+
 def path_finding(targetx, targety, depth):
 
-    # heap stores tuples: (time, x, y, tool)
-    h = [(0, 0, 0, TORCH)]
+    # heap stores tuples: (heuristic, time, x, y, tool)
+    h = [(heuristic(0, 0, 0, targetx, targety), 0, 0, 0, TORCH)]
     distance = {(0, 0, TORCH): 0}
     target = (targetx, targety, TORCH)
 
     while h:
-        time, x, y, tool = heappop(h)
+        t_h, time, x, y, tool = heappop(h)
 
-        if target in distance and distance[target] <= time:
+        if target in distance and distance[target] <= t_h:
             return distance[target]
 
         if time > distance[(x, y, tool)]:
@@ -81,7 +78,9 @@ def path_finding(targetx, targety, depth):
 
             if (x, y, tool) not in distance or time < distance[(x, y, tool)]:
                 distance[(x, y, tool)] = time
-                heappush(h, (time, x, y, tool))
+                heappush(h,
+                    (heuristic(time, x, y, targetx, targety), time, x, y, tool)
+                    )
 
 if __name__=="__main__":
     with open("22_cave/input.txt", 'r') as f:
